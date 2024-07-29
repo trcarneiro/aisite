68fd612934bd0722b87a10ad5de889529820f705
Thiago Carneiro <trcampos@gmail.com>
2024-07-29 19:09:45 +0000

    V0.01

--- backend/models.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Column, Integer, String
-from .database import Base
-
-class Article(Base):
-    __tablename__ = "articles"
-    id = Column(Integer, primary_key=True, index=True)
-    title = Column(String(255), index=True)  
-    content = Column(String(5000), index=True) 
-    keyword = Column(String(255), index=True) 
